9638609247fe2a2ccdf5167143f0d57a4938129d
JerryPC <jeremaios@gmail.com>
2021-06-01 14:13:49 -0300

    change env variables

--- config/firestore.js
+++ config/firestore.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 const serviceAccount = require('../serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    serviceAccount || process.env.SERVICE_ACCOUNT
+  ),
 });
 
 module.exports = db = admin.firestore();
